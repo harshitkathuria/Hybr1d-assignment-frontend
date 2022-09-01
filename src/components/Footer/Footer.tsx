af0e600993f6a73373e25cf93134c33a365ee9d5
harshitkathuria <harshitkathuria14@gmail.com>
2022-09-02 00:21:48 +0530

    Add Link to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import GithubLogo from "assets/GithubLogo.svg";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>Made with ❤️ by Harshit Kathuria</footer>
+    <footer className={styles.footer}>
+      <div className={styles.footer_wrapper}>
+        <div>&copy; Copyright 2022</div>
+        <div>Made with ❤️ by Harshit Kathuria</div>
+        <div className={styles.icons}>
+          <a
+            href="https://github.com/harshitkathuria/Hybr1d-assignment-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={GithubLogo} alt="GithubLogo" />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
