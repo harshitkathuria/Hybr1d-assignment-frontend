24ff8dbeec1c3d67285fb40b80498dd396556411
harshitkathuria <harshitkathuria14@gmail.com>
2022-09-01 23:53:25 +0530

    Add Loader and Handle Invalid Cases

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -2,24 +2,38 @@ import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import NewsItem from "components/NewsItem/NewsItem";
 import styles from "./News.module.scss";
+import Loader from "components/Loader/Loader";
 
 const News = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
+
   useEffect(() => {
+    if (!query) return;
+    setLoading(true);
     fetch(`https://hn.algolia.com/api/v1/search?query=${query}`)
       .then(res => res.json())
-      .then(data => setData(data.hits));
+      .then(data => {
+        setData(data.hits);
+        setLoading(false);
+      });
   }, [query]);
 
-  return (
+  return loading ? (
+    <Loader />
+  ) : (
     <div className={styles.container}>
-      <div className={styles.news}>
-        {data.length > 0
-          ? data.map((item, index) => <NewsItem data={item} key={index} />)
-          : "No data to show"}
-      </div>
+      {!query ? (
+        <div className={styles.error}>No Data to Show</div>
+      ) : (
+        <div className={styles.news}>
+          {data.length > 0
+            ? data.map((item, index) => <NewsItem data={item} key={index} />)
+            : "No data to show"}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/NewsDetail/NewsDetail.tsx
+++ src/components/NewsDetail/NewsDetail.tsx
@@ -4,54 +4,70 @@ import moment from "moment";
 import styles from "./NewsDetail.module.scss";
 import Comments from "components/Comments/Comments";
 import OpenLink from "assets/OpenLink.svg";
+import Loader from "components/Loader/Loader";
 
 const NewsDetail = () => {
   const [newsData, setNewsData] = useState<any>({});
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
+
   useEffect(() => {
+    if (!id) return;
+    setLoading(true);
     fetch(`https://hn.algolia.com/api/v1/items/${id}`)
       .then(res => res.json())
-      .then(data => setNewsData(data));
-  }, []);
+      .then(data => {
+        setNewsData(data);
+        setLoading(false);
+      });
+  }, [id]);
 
-  return newsData ? (
+  return loading ? (
+    <Loader />
+  ) : (
     <div>
-      <div className={styles.card}>
-        <div className={styles.row}>
-          <div className={styles.title}>
-            <h3>{newsData.title}</h3>
+      {!id ? (
+        <div className={styles.error}>No Data to Show</div>
+      ) : (
+        <div className={styles.card}>
+          <div className={styles.row}>
+            <div className={styles.title}>
+              <h3>{newsData.title}</h3>
+            </div>
+            <div className={styles.image}>
+              <a href={newsData.url} target="_blank" rel="noreferrer">
+                <img src={OpenLink} alt="OpenLink" />
+              </a>
+            </div>
           </div>
-          <div className={styles.image}>
-            <a href={newsData.url} target="_blank">
-              <img src={OpenLink} alt="OpenLink" />
-            </a>
-          </div>
-        </div>
-        <div className={styles.cardBody}>
-          <p>
-            {moment(newsData.created_at).format("DD MMM YYYY")}, By{" "}
-            <span className={styles.author}>{newsData.author}</span>
-          </p>
-          <div className={styles.tags}>
-            {newsData.points && (
-              <div className={styles.tag}>{newsData.points} points</div>
-            )}
-            {newsData.num_comments && (
-              <div className={styles.tag}>{newsData.num_comments} comments</div>
-            )}
-          </div>
-          <div className={styles.comments_wrapper}>
-            <p className={styles.title}>
-              Comments - {newsData.children?.length}
+          <div className={styles.cardBody}>
+            <p>
+              {moment(newsData.created_at).format("DD MMM YYYY")}, By{" "}
+              <span className={styles.author}>{newsData.author}</span>
             </p>
-            <div className={styles.comments}>
-              <Comments comments={newsData.children} />
+            <div className={styles.tags}>
+              {newsData.points && (
+                <div className={styles.tag}>{newsData.points} points</div>
+              )}
+              {newsData.num_comments && (
+                <div className={styles.tag}>
+                  {newsData.num_comments} comments
+                </div>
+              )}
+            </div>
+            <div className={styles.comments_wrapper}>
+              <p className={styles.title}>
+                Comments - {newsData.children?.length}
+              </p>
+              <div className={styles.comments}>
+                <Comments comments={newsData.children} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
-  ) : null;
+  );
 };
 
 export default NewsDetail;
